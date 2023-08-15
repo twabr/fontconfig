cea1e8f88e0e9da0d05850a6e148a1859e242a9a
WY <2628895366@qq.com>
2023-08-15 20:29:13 +0800

    first commit

---
+++ fontconfig-2.14.2/src/fcstdint.h
@@ -0,0 +1,9 @@
+#ifndef _FONTCONFIG_SRC_FCSTDINT_H
+#define _FONTCONFIG_SRC_FCSTDINT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "fontconfig 2.14.2"
+/* generated using gnu compiler emcc (Emscripten gcc/clang-like replacement + linker emulating GNU ld) 3.1.44 (bec42dac7873903d09d713963e34020c22a8bd2d) */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
